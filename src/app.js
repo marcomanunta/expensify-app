7e54d98d95e63e443a8c591c9e38dddea3436bfd
frozenborder <frozenborder@gmail.com>
2018-05-01 10:57:01 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import 'normalize.css/normalize.css';
+import 'react-dates/lib/css/_datepicker.css';
+import './styles/styles.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect, Provider } from 'react-redux';
+
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 450 }));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(
+    jsx,
+    document.getElementById('app')
+);
